04b4ec471927d07f68d2853ccf294936efc6db28
gabriel <gmoraes.mat@gmail.com>
2023-09-03 22:58:45 -0300

    Criação arquivo Database e deploy em Mongodb e Render

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(router)
+dotenv.config()
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
