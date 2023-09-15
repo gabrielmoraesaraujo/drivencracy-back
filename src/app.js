ad779d7aa9f08582b204f7e13ac66b3aed51f473
gabriel <gmoraes.mat@gmail.com>
2023-09-15 05:39:00 -0300

    Correção de conexão para deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-// app.use(router)
 dotenv.config()
 app.use(pollRouter)
 app.use(choiceRouter)
@@ -19,4 +18,7 @@ app.use(voteRouter)
 app.use(resultRouter)
 
 
-app.listen(5000, () => console.log('App rodando na porta 5000'))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
